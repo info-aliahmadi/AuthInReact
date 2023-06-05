48ca94647c465d167afbf257a62f17a57f37b480
info.aliahmadi <info.aliahmadi@gmail.com>
2023-06-05 07:38:05 +0330

    Restricted component

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,5 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import logo from "logo.svg";
+import Authorize from "services/Auth/Authorization/Authorize";
 function Admin() {
   return (
     <>
@@ -7,6 +8,10 @@ function Admin() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Welcome to Dashboard</h1>
+
+          <Authorize permission="SuperAdmin">
+            <div> Restricted element </div>
+          </Authorize>
         </header>
       </div>
     </>
